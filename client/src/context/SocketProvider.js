2495b67469a64e5e9b15d8f6c96bf8ebb5619271
DimitriNakos <dimi1nakos@gmail.com>
2021-01-21 13:07:18 -0500

    adding an existing project to github that was previously not on there.

---
+++ client/src/context/SocketProvider.js
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect, useState } from 'react';
+import io from "socket.io-client";
+
+const SocketContext = React.createContext();
+
+
+export function useSocket() {
+    return useContext(SocketContext);
+};
+
+export function SocketProvider({id, children}) {
+    const [socket, setSocket] = useState();
+
+    useEffect(() => {
+        const newSocket = io("/localhost:4000", 
+        {query: { id } })
+        console.log("Hello")
+        setSocket(newSocket);
+
+        return () => newSocket.close();
+    }, [id])
+
+return(
+    <SocketContext.Provider value={socket}>
+        {children}
+    </SocketContext.Provider>
+)
+}
